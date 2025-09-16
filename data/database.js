01edcff4b74be90562271fa24e2baf542c8e1707
Shailesh Adole <adoleshailesh2@gmail.com>
2025-09-16 18:49:33 +0530

    Changed the name of DB to WeFund to avoid confusion

--- data/database.js
+++ data/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
-      dbName: "CrowdfundingXXX_6July2024",
+      dbName: "WeFund",
     })
     .then((c) => console.log(`✅ MongoDB Connected: ${c.connection.host}`))
     .catch((e) => console.error("❌ MongoDB Atlas Connection Error:", e.message));
